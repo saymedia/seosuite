45aabf47dac0f8920b59372a0251dc02c23f58cb
Kyle Derkacz <kderkacz@saymedia.com>
2014-04-16 15:52:21 -0700

    Adding schema file

---
+++ schema.sql
@@ -0,0 +1,71 @@
+CREATE TABLE `crawl_links` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `run_id` varchar(36) NOT NULL DEFAULT '',
+  `request_hash` varchar(32) DEFAULT NULL,
+
+  # request data
+  `from_id` int(10) unsigned NOT NULL,
+  `to_id` int(10) unsigned NOT NULL,
+  `link_text` varchar(1024) DEFAULT NULL,
+  `alt_text` varchar(1024) DEFAULT NULL,
+  `rel` varchar(1024) DEFAULT NULL,
+
+  PRIMARY KEY (`id`),
+  KEY `run_id` (`run_id`),
+  KEY `from_id` (`from_id`),
+  KEY `to_id` (`to_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `crawl_urls` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `run_id` varchar(36) NOT NULL DEFAULT '',
+  `level` tinyint(4) unsigned NOT NULL DEFAULT '0',
+  `request_hash` varchar(32) DEFAULT NULL,
+  `content_hash` varchar(32) DEFAULT NULL,
+
+  # request data
+  `address` varchar(2048) NOT NULL DEFAULT '',
+  `domain` varchar(128) DEFAULT NULL,
+  `path` varchar(2048) NOT NULL DEFAULT '',
+  `external` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `status_code` tinyint(4) unsigned DEFAULT NULL,
+  `status` varchar(32) DEFAULT NULL,
+  `body` blob,
+  `size` int(10) unsigned DEFAULT NULL,
+  `address_length` int(10) unsigned NOT NULL,
+  `encoding` varchar(16) NOT NULL DEFAULT '',
+  `content_type` varchar(64) DEFAULT NULL,
+  `response_time` float unsigned DEFAULT NULL,
+  `redirect_uri` varchar(2048) DEFAULT NULL,
+  `canonical` varchar(2048) DEFAULT NULL,
+
+  # parse data
+  `title_1` varchar(1024) DEFAULT NULL,
+  `title_length_1` int(10) unsigned DEFAULT NULL,
+  `title_occurences_1` int(10) unsigned DEFAULT NULL,
+  `meta_description_1` varchar(2048) DEFAULT NULL,
+  `meta_description_length_1` int(10) unsigned DEFAULT NULL,
+  `meta_description_occurrences_1` int(10) unsigned DEFAULT NULL,
+  `h1_1` varchar(2048) DEFAULT NULL,
+  `h1_length_1` int(10) unsigned DEFAULT NULL,
+  `h1_2` varchar(2048) DEFAULT NULL,
+  `h1_length_2` int(10) unsigned DEFAULT NULL,
+  `h1_count` int(10) unsigned DEFAULT NULL,
+  `meta_robots` varchar(16) DEFAULT NULL,
+  `rel_next` varchar(2048) DEFAULT NULL,
+  `rel_prev` varchar(2048) DEFAULT NULL,
+
+  # link data
+  # `inlinks` int(10) unsigned DEFAULT NULL,
+  # `outlinks` int(10) unsigned DEFAULT NULL,
+  # `external_outlinks` int(10) unsigned DEFAULT NULL,
+
+  # lint data
+  `lint_critical` int(10) unsigned DEFAULT NULL,
+  `lint_error` int(10) unsigned DEFAULT NULL,
+  `lint_warn` int(10) unsigned DEFAULT NULL,
+  `lint_info` int(10) unsigned DEFAULT NULL,
+  `lint_results` text,
+  PRIMARY KEY (`id`),
+  KEY `run_id` (`run_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
